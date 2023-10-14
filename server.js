d56323ad82544f1a6f174a85b96f011b098fc1b3
millerdotkyle <kylenotfound@gmail.com>
2023-10-14 02:44:23 -0400

    added port to ENV

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+console.log("PORT", PORT);
 
 const hbs = exphbs.create({ helpers });
 
